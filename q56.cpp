8ed761d3b9ed5e2c42ec8c8cd8120fd14fb5e572
Hongcheng Wei <homelet.wei@mail.utoronto.ca>
2025-01-08 23:14:49 -0500

    daily leetcode 2025-01-08

--- q56.cpp
+++ q56.cpp
@@ -9,6 +9,41 @@
 
 using namespace std;
 
-int main() {
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals) {
+        // sort by starting position,
+        // similar to q452
+        std::sort(intervals.begin(), intervals.end(), [](const auto &p1, const auto &p2) {
+            return p1[0] < p2[0];
+        });
+        print_v(intervals);
+        vector<vector<int>> merged_intervals;
+        int current_pos = intervals[0][1], last_pos = intervals[0][0];
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] > current_pos) {
+                // record the last run
+                merged_intervals.emplace_back(vector<int>{last_pos, current_pos});
+                // start a new run
+                last_pos = intervals[i][0];
+                current_pos = intervals[i][1];
+            } else {
+                // extend the last run
+                last_pos = min(last_pos, intervals[i][0]);
+                current_pos = max(current_pos, intervals[i][1]);
+            }
+        }
+        // record the last run
+        merged_intervals.emplace_back(vector<int>{last_pos, current_pos});
+        return merged_intervals;
+    }
+};
 
+int main() {
+    vector<vector<int>> vec{{2, 3},
+                            {4, 5},
+                            {6, 7},
+                            {8, 9},
+                            {1, 10}};
+    print_v(Solution().merge(vec));
 }
