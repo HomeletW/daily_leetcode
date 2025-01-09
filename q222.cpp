e8441dcc55f0f598671d1eda01323d95581b8ed1
Hongcheng Wei <homelet.wei@mail.utoronto.ca>
2025-01-09 15:29:41 -0500

    daily leetcode 2025-01-09

--- q222.cpp
+++ q222.cpp
@@ -6,9 +6,41 @@
  */
 
 #include "my_util.hpp"
+#include "my_tree_node.hpp"
 
 using namespace std;
 
-int main() {
+using TreeNode = TreeNode_generic<int>;
 
+class Solution {
+public:
+    int countNodes(TreeNode *root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        TreeNode *left = root, *right = root;
+        int left_height = 0, right_height = 0;
+
+        while (left != nullptr) {
+            ++left_height;
+            left = left->left;
+        }
+
+        while (right != nullptr) {
+            ++right_height;
+            right = right->right;
+        }
+
+        if (left_height == right_height) {
+            // is a complete tree
+            return std::pow(2, left_height) - 1;
+        } else {
+            return countNodes(root->left) + countNodes(root->right) + 1;
+        }
+    }
+};
+
+int main() {
+    auto p = make_tree_node<int>(-1, {1, 2, 3, 4, 5, 6, -1});
+    print_v(*p, Solution().countNodes(p.get()));
 }
