6e13316b844b8ac5684d9cd80bc8b71b84a6ad4a
Hongcheng Wei <homelet.wei@mail.utoronto.ca>
2025-01-08 23:15:17 -0500

    daily leetcode 2025-01-08

--- q452.cpp
+++ q452.cpp
@@ -9,6 +9,27 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>> &points) {
+        // sort by the ending order
+        std::sort(points.begin(), points.end(), [](const auto &p1, const auto &p2) {
+            return p1[1] < p2[1];
+        });
+        // the idea is that every balloon must be shot down with an arrow
+        int arrow_count = 1;
+        int arrow_pos = points[0][1];
+        for (int i = 1; i < points.size(); ++i) {
+            if (points[i][0] <= arrow_pos) {
+                continue;
+            }
+            arrow_count += 1;
+            arrow_pos = points[i][1];
+        }
+        return arrow_count;
+    }
+};
+
 int main() {
 
 }
